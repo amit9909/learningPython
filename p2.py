b8615c951ef07529a5cc71552868bafebfb024ea
amit9909 <kairati0553@gmail.com>
2024-09-20 14:16:03 +0530

    arithmetic operation

---
+++ p2.py
@@ -0,0 +1,11 @@
+# Write a python program to do arithmetical addition and Division
+num1= eval(input("Enter the first number : "))
+num2= eval(input("Enter the second number : "))
+add=num1 + num2
+print("The addition of two number is : ", add)
+
+
+num1= eval(input("Enter the first number : "))
+num2= eval(input("Enter the second number : "))
+div=num1 / num2
+print("The division of two number is : ", div)
